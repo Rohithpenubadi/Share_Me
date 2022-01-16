f33aec5f27f07c5bb397f22a9475bf774148e5a6
unknown <rohithpenubadi@gmail.com>
2022-01-16 14:32:52 +0530

    first commit

---
+++ shareme_frontend/src/Components/Feed.jsx
@@ -0,0 +1,45 @@
+import React, {useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom';
+
+import { client } from '../client';
+import { feedQuery, searchQuery } from '../utils/data';
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
+
+const Feed = () => {
+
+    const [loading, setLoading] = useState(false);
+    const [pins, setPins] = useState(null)
+    const { Rohith } = useParams();
+
+    useEffect(() => {
+        setLoading(true);
+
+        if(Rohith){
+            const query = searchQuery(Rohith);
+
+            client.fetch(query)
+                .then((data) => {
+                    setPins(data)
+                    setLoading(false)
+                })
+        } else {
+            client.fetch(feedQuery).then((data) => {
+                setPins(data);
+                setLoading(false)
+            })
+        }
+        
+    }, [Rohith])
+
+    if(loading) return <Spinner message="we are adding new ideas to your feed!" />
+
+    if(!pins?.length) return <h2>No pins available</h2>
+    return (
+        <div>
+            {pins && <MasonryLayout pins={pins} />}
+        </div>
+    )
+}
+
+export default Feed
